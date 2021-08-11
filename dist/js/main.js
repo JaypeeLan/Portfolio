2906c0a0eae82ea59f198983f14ce7a5c26805f3
JaypeeLan <jplaniran01@gmail.com>
2021-08-11 17:45:46 +0100

    init

---
+++ dist/js/main.js
@@ -0,0 +1,27 @@
+const menuBtn = document.querySelector('.menu-btn');
+const mobile = document.querySelector('.menu-btn_mobile');
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.menu-nav_item');
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if(!showMenu) {
+     mobile.classList.add('open');
+     nav.classList.add('open');
+     menuNav.classList.add('open');
+     navItems.forEach(item => item.classList.add('open'));
+
+        showMenu = true;
+    }else {
+     mobile.classList.remove('open');
+     nav.classList.remove('open');
+     menuNav.classList.remove('open');
+     navItems.forEach(item => item.classList.remove('open'));
+
+        showMenu = false;
+    }
+}
