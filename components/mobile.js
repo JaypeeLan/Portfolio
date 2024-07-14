42616101d997d8d984cecb3ff9ec0f15ab7b4ba0
Laniran JohnPaul <jplaniran01@gmail.com>
2024-07-14 08:05:55 +0100

    slider

--- components/mobile.js
+++ components/mobile.js
@@ -1,26 +1,9 @@
 import React from "react";
 import Slider from "react-slick";
-import { ChevronLeft, ChevronRight } from "react-feather";
 import Card from "../components/card";
 import { PROJECTS } from "../data";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
-const NextArrow = ({ onClick }) => {
-  return (
-    <div className="slick-arrow slick-next" onClick={onClick}>
-      <ChevronRight size={32} />
-    </div>
-  );
-};
-
-const PrevArrow = ({ onClick }) => {
-  return (
-    <div className="slick-arrow slick-prev" onClick={onClick}>
-      <ChevronLeft size={32} />
-    </div>
-  );
-};
 
 const settings = {
   dots: false,
@@ -30,8 +13,6 @@ const settings = {
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 3000,
-  nextArrow: <NextArrow />,
-  prevArrow: <PrevArrow />,
 };
 
 export default function ProjectsMobile() {
